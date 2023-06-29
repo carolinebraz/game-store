ed749ae85f209ce93d33ccd10ec34deca01707fd
carolinebraz <jcarolinebraz@gmail.com>
2023-06-29 19:33:15 -0300

    Adiciona as classes Produto e Categoria na camada model

---
+++ src/main/java/com/generation/gamestore/model/Categoria.java
@@ -0,0 +1,21 @@
+package com.generation.gamestore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo categoria não pode ser nulo nem conter somente espaços em branco!")
+	private String categoria;
+	
+}
---
+++ src/main/java/com/generation/gamestore/model/Produto.java
@@ -0,0 +1,46 @@
+package com.generation.gamestore.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome não pode ser nulo nem conter somente espaços em branco!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo desenvolvedor não pode ser nulo nem conter somente espaços em branco!")
+	private String desenvolvedor;
+	
+	@NotBlank(message = "O atributo plataforma não pode ser nulo nem conter somente espaços em branco!")
+	private String plataforma;
+	
+	@NotBlank(message = "O atributo gênero não pode ser nulo nem conter somente espaços em branco!")
+	private String genero;
+	
+	@NotNull(message = "O atributo preço não pode ser nulo!")
+	@Positive(message = "O atributo preço deve ser maior que zero!")
+	private BigDecimal preco;
+	
+	@NotNull(message = "O atributo estoque não pode ser nulo!")
+	@PositiveOrZero(message = "O atributo estoque deve ser maior ou igual a zero!")
+	private int estoque;
+	
+	private LocalDate lancamento;
+
+}
